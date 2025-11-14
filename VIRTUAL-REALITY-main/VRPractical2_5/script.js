82735145d159d3cb732ea5993c51f4b9e6b630ba
AlexW-project <alexanderw124@nycstudents.net>
2025-11-14 15:28:59 +0000

    lala

--- VIRTUAL-REALITY-main/VRPractical2_5/script.js
+++ VIRTUAL-REALITY-main/VRPractical2_5/script.js
@@ -34,6 +34,19 @@ let scene;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   for(let r = 0; r < maze.length; r++){
+      for(let r = 0; r < maze.length; r++){
+    let row = maze[r];
+    let cols = row.split("");
+    for(let c = 0; c < cols.length; c++){
+      if(cols[c] == "x"){
+        new Block(c,1,r)
+      }
+      else if(cols[c] == "T"){
+        new Tree(c,1,r);
+      }else if(cols[c]=="o"){
+        new Rock(c,1,r)
+      }
+    }
     /* Challenge 3
       Choose a technique to traverse the each character in the string.
     */ 
